b4f225729a146ede5e59d195c05be31713501f3b
EternalNyte <l.brandon63@gmail.com>
2024-01-25 04:49:13 -0800

    adding job api to node

--- backend/src/controllers/jobController.ts
+++ backend/src/controllers/jobController.ts
@@ -1,15 +1,41 @@
 import { Request, Response } from 'express';
 import { connection } from '../db';
-
-interface JobResult {
-  id: number;
-  name: string;
-  short_name: string;
-  role: string;
-}
+import { RowDataPacket } from 'mysql2/promise';
 
 export const getJobs = (req: Request, res: Response) => {
-	connection.query('SELECT * FROM jobs', (error: any, results: JobResult[]) => {
+  const jobIds = req.query.job_ids as string;
+  if (jobIds) {
+    const ids = jobIds.split(',').map((id) => parseInt(id, 10));
+    connection.query('SELECT * FROM jobs WHERE id IN (?)', [ids], (error: any, results: RowDataPacket[]) => {
+      if (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      res.json(results);
+    });
+  } else {
+    connection.query('SELECT * FROM jobs', (error: any, results: RowDataPacket[]) => {
+      if (error) {
+        return res.status(500).json({ error: 'Internal Server Errors' });
+      }
+      res.json(results);
+    });
+  }
+};
+
+
+export const getJob = (req: Request, res: Response) => {
+  const jobId = req.params.job_id;
+  connection.query('SELECT * FROM jobs WHERE id=?', [jobId], (error: any, results: RowDataPacket[]) => {
+    if (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.json(results);
+  });
+};
+
+export const getJobAbilities = (req: Request, res: Response) => {
+  const jobId = req.params.job_id;
+	connection.query('SELECT * FROM abilities WHERE job_id=?', [jobId], (error: any, results: RowDataPacket[]) => {
     if (error) {
       return res.status(500).json({ error: 'Internal Server Errors' });
     }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { getAbilities } from './controllers/abilityController';
-import { getJobs } from './controllers/jobController';
+import { getJobs, getJob, getJobAbilities } from './controllers/jobController';
 
 const app = express();
 const port = 3001;
@@ -10,6 +10,8 @@ app.use(cors());
 
 app.get('/api/abilities', getAbilities);
 app.get('/api/jobs', getJobs);
+app.get('/api/jobs/:job_id', getJob);
+app.get('/api/jobs/:job_id/abilities', getJobAbilities);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
