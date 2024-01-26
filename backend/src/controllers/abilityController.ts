4e76a3febfa8742d5cbcf1691aeb07da171f4614
EternalNyte <l.brandon63@gmail.com>
2024-01-26 02:02:12 -0800

    adding query for job_ids

--- backend/src/controllers/abilityController.ts
+++ backend/src/controllers/abilityController.ts
@@ -3,11 +3,22 @@ import { connection } from '../db';
 import { RowDataPacket } from 'mysql2/promise';
 
 export const getAbilities = (req: Request, res: Response) => {
-	connection.query('SELECT * FROM abilities', (error: any, results: RowDataPacket[]) => {
-    if (error) {
-      return res.status(500).json({ error: 'Internal Server Errors' });
-    }
-    res.json(results);
-  });
+  const jobIds = req.query.job_ids as string;
+  if (jobIds) {
+    const ids = jobIds.split(',').map((id) => parseInt(id, 10));
+    connection.query('SELECT * FROM abilities WHERE job_id IN (?)', [ids], (error: any, results: RowDataPacket[]) => {
+      if (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      res.json(results);
+    });
+  } else {
+    connection.query('SELECT * FROM abilities', (error: any, results: RowDataPacket[]) => {
+      if (error) {
+        return res.status(500).json({ error: 'Internal Server Errors' });
+      }
+      res.json(results);
+    });
+  }
 };
 
